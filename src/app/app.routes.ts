a510128c083e6edab98a6a8606fd4ed84401b64b
Spartan252 <santiagoeli.ag@gmail.com>
2025-11-11 22:05:03 -0600

    Header, Login, Registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { GamePageTemplate } from './pages/game-page-template/game-page-template';
 import { NotFound } from './pages/not-found/not-found';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { UserSettings } from './pages/user-settings/user-settings';
 
 export const routes: Routes = [
     {path: "", redirectTo: "home", pathMatch:"full"},
     {path: "home", component: Home},
     {path: "games/:game", component: GamePageTemplate},
+    {path: 'login',component: Login},
+    {path: 'register',component: Register},
+    {path: 'user-settings',component: UserSettings},
     {path:"**", component: NotFound},
 ];
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,15 @@
 // Angular
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 // Matirials
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 //Services
 
@@ -16,15 +20,45 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatCardModule, MatDividerModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
 
   cuenta: number = 10;
-  isLogeado: boolean = false
+  isLogeado: boolean = true
 
-  constructor(){}
+  //---- Info del usuario HardCodeada
+  usuario = {
+    nombre: 'PptoClvUnClvto',
+    email: 'enlacalva@deuncalvito.com',
+    avatar: 'https://lumiere-a.akamaihd.net/v1/images/darth-vader-main_4560aff7.jpeg?region=0%2C67%2C1280%2C720'
+  };
+
+
+  constructor(private router: Router){}
+
+  irAHome() {
+    this.router.navigate(['/home']);
+    console.log('HOME');
+  }
+
+  irALogin() {
+    this.router.navigate(['/login']);
+    console.log('LOGIN');
+  }
+
+  irAConfiguracion() {
+    this.router.navigate(['/user-settings']);
+    console.log('SETTINGS');
+  }
+
+  cerrarSesion() {
+    //---- Simular cerrar sesión
+    console.log('BABAY');
+    this.isLogeado = false;
+    this.router.navigate(['/login']);
+  }
 
 }
