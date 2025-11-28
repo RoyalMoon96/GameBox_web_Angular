fe8d4b5e73971bd9da57c3924a1caf25103f20c8
RoyalMoon96 <diegoo_doo@hotmail.com>
2025-11-27 20:24:45 -0600

    stats

--- src/app/pages/stats/stats.ts
+++ src/app/pages/stats/stats.ts
@@ -12,6 +12,7 @@ import { StatsService } from '../../shared/services/stats/stats-service';
 
 //Components
 import { IMatch } from '../../shared/types/imatch';
+import { UserService } from '../../shared/services/user/user-service';
 
 @Component({
   selector: 'app-stats',
@@ -22,11 +23,13 @@ import { IMatch } from '../../shared/types/imatch';
 export class Stats implements OnInit{
   matches:IMatch[]=[]
   player_name:String=""
-  wins:number=1
-  constructor (private statsService:StatsService){}
+  wins: Number = 0
+  constructor (private statsService:StatsService, private userService: UserService){}
   ngOnInit(): void {
-    this.player_name="User1"
-    this.matches=this.statsService.getStats()
-    this.wins= this.matches.filter((m)=>m.winer==this.player_name).length
+    this.player_name 
+    this.statsService.getStats().subscribe(data => {
+      this.matches = data.stats;
+      this.wins = data.wins;
+    });
   }
 }
--- src/app/shared/services/stats/stats-service.ts
+++ src/app/shared/services/stats/stats-service.ts
@@ -1,44 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { IMatch } from '../../types/imatch';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../../../../environments/environment';
+import { TokenService } from '../token/token-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StatsService {
-  matches:IMatch[]=[
-    {
-      winer: "User1",
-      date: new(Date),
-      game: "snake",
-      details: "user1(100pts), user2(50pts)"
-    },
-    {
-      winer: "User2",
-      date: new(Date),
-      game: "snake",
-      details: "user1(10pts), user2(50pts)"
-    },
-    {
-      winer: "User1",
-      date: new(Date),
-      game: "conecta4",
-      details: "user1(200pts), user2(10pts)"
-    },
-    {
-      winer: "User1",
-      date: new(Date),
-      game: "conecta4",
-      details: "user1(200pts), user2(10pts)"
-    },
-    {
-      winer: "User1",
-      date: new(Date),
-      game: "conecta4",
-      details: "user1(200pts), user2(10pts)"
-    }
-  ]
+  endpoint = '/api/stats'
 
-  getStats():IMatch[]{
-    return this.matches
-  }
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
+
+getStats(): Observable<{"stats": IMatch[], "wins": Number}> {
+  const token = this.tokenService.getToken();
+
+  const headers = new HttpHeaders({
+    Authorization: `Bearer ${token}`
+  });
+
+  return this.http.get<any>(
+    `${environment.SERVER_URL}${this.endpoint}`,
+    { headers }
+  ).pipe(
+    map(res => { 
+      console.log(res); 
+      const response = {
+        "stats": res.stats as IMatch[],
+        "wins": res.wins || 0
+       }
+      return response
+    })
+  );
+}
+
 }
